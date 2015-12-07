1fc8dbd059a3c392a43e701aa81fba6604a83500
Jason A. Storey <jason.storey@unrulymedia.com>
2015-12-07 10:25:54 -0500

    Parse basic question and asker, and stub out response

--- wanda-comms/index.js
+++ wanda-comms/index.js
@@ -2,6 +2,16 @@
 let twitterComms = require('./lib/twitter-comms');
 
 twitterComms.init('A dummy Twitter config');
+twitterComms.getEmitter().on('question', question => {
+    twitterComms.respond(getResponse(question));
+});
+
+function getResponse(question) {
+    return {
+        text: 'No, @' + question.asker + ', that is a terrible idea.',
+        image: 'some image'
+    };
+}
 
 console.log('wanda comms initialised');
 
--- wanda-comms/lib/twitter-comms.js
+++ wanda-comms/lib/twitter-comms.js
@@ -1,5 +1,6 @@
 'use strict'
 
+let EventEmitter = require('events');
 let Twitter = require('Twitter');
 let CREDENTIALS = require('../credentials.json');
 
@@ -10,20 +11,44 @@ let client = new Twitter({
     access_token_secret: CREDENTIALS.access_token_secret,
 });
 
+let emitter;
+
 function init(config) {
+    emitter = new EventEmitter();
     console.log('initialising twitter comms');
     console.log(config);
     client.stream('user', {}, function(stream) {
-        stream.on('data', function(tweet) {
-            console.log(tweet);
+        stream.on('data', function(data) {
+            console.log(data);
+            if(isTweet(data)) {
+                emitter.emit('question', getQuestion(data));
+            }
         });
+
         stream.on('error', function(error) {
             throw error;
         });
     });
 }
 
+function getQuestion(tweet) {
+    return {
+        text: tweet.text,
+        asker: tweet.user.screen_name
+    };
+}
+
+function respond(response) {
+    console.log(response);
+}
+
+function isTweet(data) {
+    return data.created_at && data.user;
+}
+
 module.exports = {
-    init: init
+    init: init,
+    getEmitter: () => emitter,
+    respond: respond
 };
 

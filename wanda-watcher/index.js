70a05cfa5b5cb5225f496d8c2b63143d094687a0
Jason A. Storey <jason.storey@unrulymedia.com>
2015-12-08 16:24:29 +0000

    Wanda watcher web server with stubbed out json response

--- wanda-watcher/index.js
+++ wanda-watcher/index.js
@@ -1,7 +1,20 @@
 'use strict'
+
+let express = require('express');
+let app = express();
 let wandaWatcher = require('./lib/wanda-watcher');
 
 wandaWatcher.init('wanda watcher config');
+
+app.get('/question', (req, res) => {
+    res.json(getAnswer());
+});
+
+function getAnswer() {
+    return wandaWatcher.getState();
+}
+
+app.listen(3000);
 
 console.log('wanda watcher initialised');
 
--- wanda-watcher/lib/wanda-watcher.js
+++ wanda-watcher/lib/wanda-watcher.js
@@ -1,4 +1,19 @@
+'use strict'
+
+function init(config) {
+    console.log(config);
+}
+
+function getState() {
+    return {
+        positionId: 0,
+        image: 'wanda pic',
+        timestamp: Date.now()
+    };
+}
+
 module.exports = {
-    init: config => console.log(config)
+    init: init,
+    getState: getState
 };
 

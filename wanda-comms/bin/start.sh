ddb16e038d2b32de5fbd4070eb7fcb62fb50d4f7
Jason A. Storey <jason.storey@unrulymedia.com>
2015-12-11 19:03:56 +0000

    Improve start script for both modules

---
+++ wanda-comms/bin/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cd /home/pi/wanda/wanda-comms && node /home/pi/wanda/wanda-comms > stdout.txt 2> stderr.txt &
--- wanda-watcher/bin/start.sh
+++ wanda-watcher/bin/start.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-killall node
-
 cd /home/pi/wanda/wanda-watcher && node /home/pi/wanda/wanda-watcher > stdout.txt 2> stderr.txt &

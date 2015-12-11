2405a066d6def8d4d1dacd4a75e5a5de98a7ab18
Jason A. Storey <jason.storey@unrulymedia.com>
2015-12-11 19:57:30 +0000

    Tweak thresholds in detection algorithm

--- wanda-watcher/lib/image-analysis.js
+++ wanda-watcher/lib/image-analysis.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let getPixels = require('get-pixels');
+let imageDeltaThreshold = 2000;
 let calibratedPixels;
 
 function init() {
@@ -17,9 +18,13 @@ function findWanda(imagePath, done) {
             rightPixels = getPixelsFromQuadrant(1, pixels);
 
         let leftDelta = calculateImageDelta(getPixelsFromQuadrant(0, calibratedPixels), leftPixels),
-            rigthDelta = calculateImageDelta(getPixelsFromQuadrant(1, calibratedPixels), rightPixels);
+            rightDelta = calculateImageDelta(getPixelsFromQuadrant(1, calibratedPixels), rightPixels);
 
-        fishPosish = leftDelta > rigthDelta ? 0 : 1;
+        if(Math.max(leftDelta, rightDelta) > imageDeltaThreshold) {
+            fishPosish = leftDelta > rightDelta ? 0 : 1;
+        }
+
+        console.log(leftDelta, rightDelta, fishPosish);
 
         done(fishPosish);
     });
@@ -27,15 +32,15 @@ function findWanda(imagePath, done) {
 
 function calculateImageDelta(oldImagePixels, newImagePixels) {
     let delta = 0,
-        red = newImagePixels.pick(null, null, 0),
+        newRed = newImagePixels.pick(null, null, 0),
         oldRed = oldImagePixels.pick(null, null, 0);
 
-    for(var i = 0; i < red.shape[0]; ++i) {
+    for(var i = 0; i < newRed.shape[0]; ++i) {
         if(i % 5 === 0) {
-            for(var j = 0; j < red.shape[1]; ++j) {
+            for(var j = 0; j < newRed.shape[1]; ++j) {
                 if (j % 5 === 0) {
-                    let diff = Math.abs(oldRed.get(i, j) - red.get(i, j));
-                    delta += diff > 5 ? diff : 0;
+                    let diff = Math.abs(oldRed.get(i, j) - newRed.get(i, j));
+                    delta += diff > 40 ? diff : 0;
                 }
             }
         }
